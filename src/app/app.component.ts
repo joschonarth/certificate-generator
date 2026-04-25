3ee2008fb05ac796681a99a1c9e67cd2155f928f
joschonarth <joschonarth@gmail.com>
2026-04-25 16:07:40 -0300

    feat: create secondary-button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { CommonModule } from '@angular/common';
 import { PrimaryButtonComponent } from "./components/primary-button/primary-button.component";
+import { SecondaryButtonComponent } from "./components/secondary-button/secondary-button.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CommonModule, PrimaryButtonComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, PrimaryButtonComponent, SecondaryButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

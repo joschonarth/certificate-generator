46e8a147743a0e0518be5f8c8f471b96ee34d999
joschonarth <joschonarth@gmail.com>
2026-04-28 20:55:54 -0300

    feat: add navigation with Angular Router

--- src/app/components/certificate-item/certificate-item.component.ts
+++ src/app/components/certificate-item/certificate-item.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from '../secondary-button/secondary-button.component';
-import { RouterLink } from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-certificate-item',
   standalone: true,
-  imports: [SecondaryButtonComponent, RouterLink],
+  imports: [SecondaryButtonComponent],
   templateUrl: './certificate-item.component.html',
   styleUrl: './certificate-item.component.css'
 })
 export class CertificateItemComponent {
   id: string = '1'
+
+  constructor(private router: Router) {}
+
+  redirectCertificate() {
+    this.router.navigate(['/certificados', 2])
+  }
 }

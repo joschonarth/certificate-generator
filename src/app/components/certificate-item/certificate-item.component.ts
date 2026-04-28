c7f2dd9bb34eefcc9e5b802271073e784ae20e30
joschonarth <joschonarth@gmail.com>
2026-04-28 20:41:04 -0300

    feat: add navigation with RouterLink

--- src/app/components/certificate-item/certificate-item.component.ts
+++ src/app/components/certificate-item/certificate-item.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from '../secondary-button/secondary-button.component';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-certificate-item',
   standalone: true,
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, RouterLink],
   templateUrl: './certificate-item.component.html',
   styleUrl: './certificate-item.component.css'
 })
 export class CertificateItemComponent {
-
+  id: string = '1'
 }
--- src/app/pages/certificate/certificate.component.ts
+++ src/app/pages/certificate/certificate.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from '../../components/secondary-button/secondary-button.component';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-certificate',
   standalone: true,
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, RouterLink],
   templateUrl: './certificate.component.html',
   styleUrl: './certificate.component.css'
 })

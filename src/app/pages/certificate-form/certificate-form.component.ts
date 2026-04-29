b5033cc9e3f6e01b8a3fd516af74abda4f71da27
joschonarth <joschonarth@gmail.com>
2026-04-28 22:59:26 -0300

    feat: add form binding with ngModel

--- src/app/pages/certificate-form/certificate-form.component.ts
+++ src/app/pages/certificate-form/certificate-form.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from '../../components/secondary-button/secondary-button.component';
 import { PrimaryButtonComponent } from '../../components/primary-button/primary-button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificate-form',
   standalone: true,
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [PrimaryButtonComponent, SecondaryButtonComponent, FormsModule],
   templateUrl: './certificate-form.component.html',
   styleUrl: './certificate-form.component.css'
 })
 export class CertificateFormComponent {
-
+  name = '';
+  activity = '';
+  activities: string[] = ['Angular', 'Java', 'Spring Boot'];
 }

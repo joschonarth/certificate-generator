434f6a79a9f6c14de4ba34ab2a88c8525561fa4a
joschonarth <joschonarth@gmail.com>
2026-04-29 00:05:22 -0300

    feat: add form field validation

--- src/app/pages/certificate-form/certificate-form.component.ts
+++ src/app/pages/certificate-form/certificate-form.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from '../../components/secondary-button/secondary-button.component';
 import { PrimaryButtonComponent } from '../../components/primary-button/primary-button.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificate-form',
   standalone: true,
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent, FormsModule],
+  imports: [PrimaryButtonComponent, SecondaryButtonComponent, FormsModule, CommonModule],
   templateUrl: './certificate-form.component.html',
   styleUrl: './certificate-form.component.css'
 })
@@ -14,4 +15,8 @@ export class CertificateFormComponent {
   name = '';
   activity = '';
   activities: string[] = ['Angular', 'Java', 'Spring Boot'];
+
+  invalidField(control: NgModel) {
+    return control.invalid && control.touched;
+  }
 }
